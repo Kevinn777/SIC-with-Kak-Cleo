459d1a46caedf764a27406954f356d2f3e544779
Cleonart <zredhard@gmail.com>
2021-10-03 14:35:21 +0800

    update

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -7,22 +7,22 @@ function SignIn() {
     return (
         <main>
             <div class="logo">
-                <img src={logo} alt="" srcset=""/>
+                <img src={logo} alt="" srcset="" />
             </div>
             <div class="login-form">
-            <h1>Login</h1>
-            <form action="">
-                <label for="username">Username</label>
-                <input class="input-username" type="text" id="username" required/>
-                <label for="password">Password</label>
-                <input class="input-password" type="text" id="password" required/>
-                <button>Login</button>
-            </form>
-            <div class="help-section">
-                <a href="sign_up.html">Sign Up</a>
-                <a href="">Lupa password</a>
+                <h1>Login</h1>
+                <form action="">
+                    <label for="username">Username</label>
+                    <input class="input-username" type="text" id="username" required />
+                    <label for="password">Password</label>
+                    <input class="input-password" type="text" id="password" required />
+                    <button>Login</button>
+                </form>
+                <div class="help-section">
+                    <a href="sign_up.html">Sign Up</a>
+                    <a href="">Lupa password</a>
+                </div>
             </div>
-            </div>  
         </main>
     )
 }
